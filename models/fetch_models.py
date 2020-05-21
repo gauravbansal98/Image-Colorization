4c1567a4aa5e39666d830c044a1fbc2a6119cc07
Gaurav Bansal <gauravbansal233@gmail.com>
2020-05-21 23:49:43 +0530

    Update fetch_models.py

--- models/fetch_models.py
+++ models/fetch_models.py
@@ -1,8 +1,4 @@
 
 import wget
 
-wget.download("http://colorization.eecs.berkeley.edu/siggraph/models/model.caffemodel", "models/reference_model/model.caffemodel")
-wget.download("http://colorization.eecs.berkeley.edu/siggraph/models/global_model.caffemodel", "models/global_model/global_model.caffemodel")
-wget.download("http://colorization.eecs.berkeley.edu/siggraph/models/dummy.caffemodel", "models/global_model/dummy.caffemodel")
 wget.download("http://colorization.eecs.berkeley.edu/siggraph/models/pytorch.pth", "models/pytorch/pytorch_trained.pth")
-wget.download("http://colorization.eecs.berkeley.edu/siggraph/models/caffemodel.pth", "models/pytorch/caffemodel.pth")
